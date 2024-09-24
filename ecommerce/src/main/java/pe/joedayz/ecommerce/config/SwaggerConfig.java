31918808468b4e40bfbb31ab2ae749aa508691a0
Jhari <lkzknow@hotmail.com>
2024-09-23 19:16:13 -0500

    testing git integration

--- ecommerce/src/main/java/pe/joedayz/ecommerce/config/SwaggerConfig.java
+++ ecommerce/src/main/java/pe/joedayz/ecommerce/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class SwaggerConfig {
 
-    //new change ( Cmd + K | Ctrl + K para Win y Linux)
+    //new change ( Cmd + K | Ctrl + K para Win y Linux) "Jhan"
   @Value("${joedayz.openapi.dev-url}")
   private String devUrl;
 
